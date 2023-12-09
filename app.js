570ba7d34fdd862f7edb43d0269a85df5e2cb1b6
saadghazal <saad.m.ghazal@icloud.com>
2023-12-09 22:18:41 +0300

    Understanding Middlewares

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require('http')
+const express = require('express')
 
-const server = http.createServer()
+const app = express()
+
+// use allows us to add a new middleware function 
+// it accepts an array of request handlers
+app.use((req,res,next)=>{
+    console.log("In The Middleware")
+    next() // Allows the request to continue to the next middleware in line
+})
+app.use((req,res,next)=>{
+    console.log("In another Middleware")
+})
+
+
+const server = http.createServer(app)
 
 server.listen(3000)
