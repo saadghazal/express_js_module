21ac50c2aeee4f0543189b7f21213538b2030f2b
saadghazal <saad.m.ghazal@icloud.com>
2023-12-10 21:16:04 +0300

    Body Parsing

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+
+//it register a middleware it will parse the incoming requests bodies
+app.use(bodyParser.urlencoded({extended: false}))
 // use allows us to add a new middleware function 
 // it accepts an array of request handlers
-app.use((req,res,next)=>{
-    console.log("In The Middleware")
-    next() // Allows the request to continue to the next middleware in line
+// app.use((req,res,next)=>{
+//     console.log("In The Middleware")
+//     next() // Allows the request to continue to the next middleware in line
+// })
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type = "text" name="title"><button type="submit">Add Product</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log("In another Middleware")
+
+app.use('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+
     res.send('<h1>Hello From Express JS</h1>')
 })
 
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
