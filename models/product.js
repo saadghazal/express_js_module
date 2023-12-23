7e39fe6bdcdc00efc01425046757dde5dbe4eecc
saadghazal <saad.m.ghazal@icloud.com>
2023-12-23 12:49:20 +0300

    Refactoring our code

--- models/product.js
+++ models/product.js
@@ -1,32 +1,34 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+const p = path.join(
+    path.dirname(require.main.filename),
+    "data",
+    "products.json",
+  );
+const getProductFromFile = (callBack) => {
+  
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callBack([]);
+    }
+    callBack(JSON.parse(fileContent));
+  });
+};
 
-
-module.exports = class Product{
-    constructor(title){
-        this.title = title
-        
-    }
-    save(){
-        const p = path.join(path.dirname(require.main.filename),'data','products.json')
-        fs.readFile(p,(err,fileContent)=>{
-            let products = []
-            if(!err){
-               products = JSON.parse(fileContent) 
-            }
-            products.push(this)
-            fs.writeFile(p,JSON.stringify(products),(err)=>{
-                console.log(err)
-            })
-        })
-    }
-    static fetchAll(callBack){
-        const p = path.join(path.dirname(require.main.filename),'data','products.json')
-        fs.readFile(p,(err,fileContent)=>{
-            if(err){
-                callBack([]);
-            }
-            callBack( JSON.parse(fileContent))
-        })
-    }
-}
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    getProductFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    })
+    
+  }
+  static fetchAll(callBack) {
+    getProductFromFile(callBack)
+  }
+};
