35e710029020426b41cd332a1bd0704571cd22aa
saadghazal <saad.m.ghazal@icloud.com>
2024-01-06 20:18:38 +0300

    creating the mongodb connection

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const sequelize = require("./util/sql_database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/sql_database");
 
 const app = express();
 
@@ -27,8 +21,8 @@ Allows us to tell express where to find these dynamic views
 
 */
 
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+// const adminRouter = require("./routes/admin");
+// const shopRouter = require("./routes/shop");
 
 
 //it register a middleware it will parse the incoming requests bodies
@@ -36,71 +30,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public"))); // it will access static files that should be included in the html pages for example like a css files
 
-// use allows us to add a new middleware function
-// it accepts an array of request handlers
-// app.use((req,res,next)=>{
-//     console.log("In The Middleware")
-//     next() // Allows the request to continue to the next middleware in line
-// })
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findByPk(1)
+//     .then((user) => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.use("/admin", adminRouter);
+// app.use("/admin", adminRouter);
 
-app.use(shopRouter);
+// app.use(shopRouter);
 
 app.use(errorController.error404);
 
-/**
- * Sets up a one-to-many relationship between the Product and User models.
- * The Product model belongs to a User - a product will have a userId field that references the User it belongs to.
- * The User model has many Products - deleting a User will also delete all their associated Products due to the CASCADE option.
- */
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
 
-/**
- * Connects to the database using Sequelize and starts the Express app listening on port 3000.
- * On success, starts the app listening on port 3000.
- * On error, logs the error.
- */
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: "Max",
-        email: "a@b.c",
-      });
-    }
-    return Promise.resolve(user);
-  })
-  
-  .then((cart) => {
-    app.listen(3000);
-
-  })
-  .catch((err) => {
-    console.log(err);
+mongoConnect(client =>{
+  console.log(client);
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
   });
+})
