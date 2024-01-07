0d6cccfd1dd41461586a8658e5afc06a88eaa7d2
saadghazal <saad.m.ghazal@icloud.com>
2024-01-07 11:20:24 +0300

    Fetching admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -40,29 +40,29 @@ module.exports.postAddProduct = (req, res) => {
  * Looks up the product by ID, and renders the edit page with the product data.
  * Redirects to homepage if edit mode is not enabled or product not found.
  */
-// module.exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
-//   const productId = req.params.productId;
-//   req.user.getProducts({where: {id: productId}})
-//     .then((products) => {
-//       const product = products[0];
-//       if (!product) {
-//         return res.redirect("/");
-//       }
-//       res.render("admin/edit-product", {
-//         pageTitle: "Edit Product",
-//         path: "/admin/edit-product",
-//         editing: true,
-//         product: product,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+module.exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const productId = req.params.productId;
+  req.user.getProducts({where: {id: productId}})
+    .then((products) => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: true,
+        product: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 /**
  * Updates a product in the database.
@@ -104,7 +104,7 @@ module.exports.postEditProduct = (req, res, next) => {
  * @param {Function} next - Express next middleware function
  */
 module.exports.getProducts = (req, res, next) => {
-  req.user.getProducts()
+  Product.fetchAll()
     .then((products) => {
       res.render("admin/products", {
         products_list: products,
--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get("/add-product", adminController.getAddProduct);
 router.post("/add-product", adminController.postAddProduct);
 
 // router.get("/edit-product/:productId", adminController.getEditProduct);
-// router.get("/products",adminController.getProducts)
+router.get("/products",adminController.getProducts)
 
 // router.post("/edit-product", adminController.postEditProduct);
 // router.post("/delete-product", adminController.postDeleteProduct);
